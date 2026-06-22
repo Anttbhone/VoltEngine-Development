471562142b5b8278e55e1efb6e8dd93475562171
Antt Bhone Yaung Thwe <135525382+Anttbhone@users.noreply.github.com>
2026-06-22 10:35:14 +0000

    Added a tokenizer.

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -2,9 +2,12 @@
 #include <vector>
 
 int main() {
-    std::vector<float> vector = {1,2,3,4};
-    for (int i = 0; i < vector.size; i++;) {
-        vector[i] += 5;
+    std::vector<int> vector;
+    for (int i = 0; i < 10; i++) {
+        vector.push_back(i);
+    }
+    for (int i = 0; i < 10; i++) {
+        vector[i] *= 2;
     }
     return 0;
 }

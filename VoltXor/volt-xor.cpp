34f27d7d1de58d8b90be1270bed69c718194db00
Antt Bhone Yaung Thwe <135525382+Anttbhone@users.noreply.github.com>
2026-06-28 05:59:05 +0000

    Added Foward Pass and MSE Loss to volt-xor.cpp

--- VoltXor/volt-xor.cpp
+++ VoltXor/volt-xor.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <cmath>
 int get_index(int row, int col, int total_col) {
     int index = (row * total_col) + col;
     return index;
+}
+float sigmoid(float x) {
+    return 1.0f / (1.0f + std::exp(-x));
 }
 int main() {
     int i = 0;
@@ -26,6 +30,12 @@ int main() {
     std::vector<float> b1(3, 0.0f);
     std::vector<float> b2(1, 0.0f);
     std::vector<float> hidden_layer(12, 0.0f);
+    std::vector<float> output_layer(4, 0.0f);
+    std::vector<float> dw1(6, 0.0f);
+    std::vector<float> dw2(3, 0.0f);
+    std::vector<float> db1(3, 0.0f);
+    std::vector<float> db2(1, 0.0f);
+    std::vector<float> output_errors(4, 0.0f);
     for (i = 0; i < w1.size(); i++) {
         w1[i] = dist(gen);
     }
@@ -52,11 +62,34 @@ int main() {
             }
             dot_product += b1[h];
             int hidden_box = get_index(r, h, 3);
-            hidden_layer[hidden_box] = dot_product;
-            
-            std::cout << "Row " << r << ", Hidden Neuron " << h 
-                      << " Raw Value: " << hidden_layer[hidden_box] << "\n";
+            hidden_layer[hidden_box] = sigmoid(dot_product);
         }
-    } 
+    }
+    std::cout << "\n--- Computing Final AI Guesses ---\n";
+    for (int r = 0; r < 4; r++) {
+        float out_dot_product = 0.0f;
+        for (int h = 0; h < 3; h++) {
+            int hidden_box = get_index(r, h, 3);
+            int weight_box = get_index(h, 0, 1); // w2 is 3 rows x 1 col
+            out_dot_product += hidden_layer[hidden_box] * w2[weight_box];
+        }
+        out_dot_product += b2[0];
+        output_layer[r] = sigmoid(out_dot_product);
+        std::cout << "Scenario " << r << " AI Guess: " << output_layer[r] 
+                  << " | Correct Target: " << xor_target[r] << "\n";
+    }
+    float total_loss = 0;
+    for (int r = 0; r < 4; r++) {
+        float error = xor_target[r] - output_layer[r];
+        total_loss += error * error;
+    }
+    total_loss /= 4.0f;
+    std::cout << "Total Loss: " << total_loss << "\n";
+    for (int r = 0; r < 4; r++) {
+        float guess = output_layer[r];
+        float target = xor_target[r];
+        output_errors[r] = (guess - target) * guess * (1.0f - guess);
+        std::cout << "Row " << r << " | Gradient Error Delta: " << output_errors[r] << "\n";
+    }
     return 0;
 }

5fb00b8367d59152c8338be6c2ed84172ad0038f
Antt Bhone Yaung Thwe <135525382+Anttbhone@users.noreply.github.com>
2026-07-14 14:20:49 +0000

    Added .at function into the Tensor to add specific float values to a flat 1D vector

--- Volt-Models/VoltEngine 1.0/src/tensor.cpp
+++ Volt-Models/VoltEngine 1.0/src/tensor.cpp
@@ -6,7 +6,14 @@ public:
     int rows;
     int cols;
     Tensor(int r, int c) : rows(r), cols(c), data(r * c, 0.0f) {}
+    float& at(int r, int c) {
+        int index = (r * cols) + c;
+        return data[index];
+    }
 };
 int main() {
+    Tensor matrix(2, 3);
+    matrix.at(1, 1) = 7.5;
+    std::cout << matrix.at(1, 1);
     return 0;
 }

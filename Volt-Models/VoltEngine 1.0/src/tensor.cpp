d7ce3d7079900d6e613f985f75bb959b549dd294
Antt Bhone Yaung Thwe <135525382+Anttbhone@users.noreply.github.com>
2026-07-14 15:12:25 +0000

    Added nested loop .print function to print out the flat 1D vector

--- Volt-Models/VoltEngine 1.0/src/tensor.cpp
+++ Volt-Models/VoltEngine 1.0/src/tensor.cpp
@@ -2,18 +2,30 @@
 #include <vector>
 class Tensor {
 public:
+
     std::vector<float> data;
+
     int rows;
     int cols;
+
     Tensor(int r, int c) : rows(r), cols(c), data(r * c, 0.0f) {}
     float& at(int r, int c) {
         int index = (r * cols) + c;
         return data[index];
     }
+
+    void print() {
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+            std::cout << at(r, c) << " ";
+            }
+        std::cout << "\n";
+        }
+    }
 };
 int main() {
     Tensor matrix(2, 3);
     matrix.at(1, 1) = 7.5;
-    std::cout << matrix.at(1, 1);
+    matrix.print();
     return 0;
 }

491ff49d9675f74fde8cea03c20d9b33c80f7637
Antt Bhone Yaung Thwe <135525382+Anttbhone@users.noreply.github.com>
2026-07-19 13:47:54 +0000

    Made a triple nested loop to make a matmul function (WIP)

--- Volt-Models/VoltEngine 1.0/src/tensor.cpp
+++ Volt-Models/VoltEngine 1.0/src/tensor.cpp
@@ -22,6 +22,28 @@ public:
         std::cout << "\n";
         }
     }
+
+    Tensor matmul(Tensor& other) {
+        if (this->cols != other.rows) {
+            std::cout << "Error: Cant perform matmul.";
+            return Tensor(0, 0);
+        }
+        int target_row = this->rows;
+        int target_col = other.cols;
+        Tensor result(target_row, target_col);
+        for (int i = 0; i < this->rows; i++) {
+            for (int j = 0; j < other.cols; j++) {
+                float sum = 0.0f;
+                for (int k = 0; k < this->cols; k++) {
+                    sum += this->at(i, k) * other.at(k, j);
+                }
+                result.at(i, j) = sum;
+            }
+        }
+        return result;
+    }
+
+
 };
 int main() {
     Tensor matrix(2, 3);

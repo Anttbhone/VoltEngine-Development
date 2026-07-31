a8f1ff3016e82a76d227e52e1c1095fec14782c1
Antt Bhone Yaung Thwe <135525382+Anttbhone@users.noreply.github.com>
2026-07-31 16:05:58 +0000

    Added Tensor.cpp for the heavy math engine

---
+++ VoltEngine/Core/Tensor.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+class Tensor {
+public:
+
+    std::vector<float> data;
+
+    int rows;
+    int cols;
+
+    Tensor::Tensor(int r, int c) : rows(r), cols(c), data(r * c, 0.0f) {}
+    const float& Tensor::at(int r, int c) {
+        int index = (r * cols) + c;
+        return data[index];
+    }
+
+    void Tensor::print() {
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+            std::cout << at(r, c) << " ";
+            }
+        std::cout << "\n";
+        }
+    }
+
+    Tensor Tensor::matmul(const Tensor& other) {
+        if (this->cols != other.rows) {
+            std::cout << "Error: Cant perform matmul.";
+            return Tensor(0, 0);
+        }
+        int target_row = this->rows;
+        int target_col = other.cols;
+        Tensor result(target_row, target_col);
+        for (int i = 0; i < this->rows; i++) {
+            for (int j = 0; j < other.cols; j++) {
+                float sum = 0.0f;
+                for (int k = 0; k < this->cols; k++) {
+                    sum += this->at(i, k) * other.at(k, j);
+                }
+                result.at(i, j) = sum;
+            }
+        }
+        return result;
+    }
+    void silu() {
+        for (int i = 0; i < data.size(); i++) {
+            float x = data[i];
+            data[i] = x * 1.0f / (1.0f + std::exp(-x));
+        }
+    }
+};
+int main() {
+    return 0;
+}

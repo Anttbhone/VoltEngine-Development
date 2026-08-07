0fd29a755f8005e843ee21e638b58ff885b362fb
Antt Bhone Yaung Thwe <135525382+Anttbhone@users.noreply.github.com>
2026-08-07 10:02:02 +0000

    Updated tensor source code to use blueprints

--- VoltEngine/Core/Tensor.cpp
+++ VoltEngine/Core/Tensor.cpp
@@ -1,55 +1,46 @@
+#include "Tensor.h"
 #include <iostream>
 #include <vector>
 #include <cmath>
-class Tensor {
-public:
 
-    std::vector<float> data;
+Tensor::Tensor(int r, int c) 
+    : rows(r), cols(c), data(r * c, 0.0f), grad(r * c, 0.0f), requires_grad(false) {}
 
-    int rows;
-    int cols;
+float& Tensor::at(int r, int c) {
+    int index = (r * cols) + c;
+    return data[index];
+}
 
-    Tensor::Tensor(int r, int c) : rows(r), cols(c), data(r * c, 0.0f) {}
-    const float& Tensor::at(int r, int c) {
-        int index = (r * cols) + c;
-        return data[index];
-    }
+float& Tensor::at_grad(int r, int c) {
+    int index = (r * cols) + c;
+    return grad[index];
+}
 
-    void Tensor::print() {
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
+void Tensor::print() {
+    for (int r = 0; r < rows; r++) {
+        for (int c = 0; c < cols; c++) {
             std::cout << at(r, c) << " ";
-            }
+        }
         std::cout << "\n";
-        }
     }
+}
 
-    Tensor Tensor::matmul(const Tensor& other) {
-        if (this->cols != other.rows) {
-            std::cout << "Error: Cant perform matmul.";
-            return Tensor(0, 0);
-        }
-        int target_row = this->rows;
-        int target_col = other.cols;
-        Tensor result(target_row, target_col);
-        for (int i = 0; i < this->rows; i++) {
-            for (int j = 0; j < other.cols; j++) {
-                float sum = 0.0f;
-                for (int k = 0; k < this->cols; k++) {
-                    sum += this->at(i, k) * other.at(k, j);
-                }
-                result.at(i, j) = sum;
+Tensor Tensor::matmul(Tensor& other) {
+    if (this->cols != other.rows) {
+        std::cout << "Error: Cant perform matmul.\n";
+        return Tensor(0, 0);
+    }
+    int target_row = this->rows;
+    int target_col = other.cols;
+    Tensor result(target_row, target_col);
+    for (int i = 0; i < this->rows; i++) {
+        for (int j = 0; j < other.cols; j++) {
+            float sum = 0.0f;
+            for (int k = 0; k < this->cols; k++) {
+                sum += this->at(i, k) * other.at(k, j);
             }
-        }
-        return result;
-    }
-    void silu() {
-        for (int i = 0; i < data.size(); i++) {
-            float x = data[i];
-            data[i] = x * 1.0f / (1.0f + std::exp(-x));
+            result.at(i, j) = sum;
         }
     }
-};
-int main() {
-    return 0;
+    return result;
 }
--- VoltEngine/Core/Tensor.h
+++ VoltEngine/Core/Tensor.h
@@ -4,10 +4,13 @@
 class Tensor {
 public:
     std::vector<float> data;
+    std::vector<float> grad;
+    bool requires_grad;
     int rows;
     int cols;
     Tensor(int r, int c);
     float& at(int r, int c);
+    float& at_grad(int r, int c);
     void print();
     Tensor matmul(Tensor& other);
 };
